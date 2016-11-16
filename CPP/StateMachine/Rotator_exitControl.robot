b4ac05af5a980a071f8609a82fc6a2ea95802821
Rob Bovill <rbovill@lsst.org>
2016-11-16 11:09:57 -0700

    Added C++ and Python state machine tests.

---
+++ CPP/StateMachine/Rotator_exitControl.robot
@@ -0,0 +1,116 @@
+*** Settings ***
+Documentation    Rotator State Machine tests.
+Suite Setup    Log Many    ${Host}    ${subSystem}    ${timeout}
+Suite Teardown    Close All Connections
+Library    SSHLibrary
+Library    String
+Resource    ../../Global_Vars.robot
+
+*** Variables ***
+${subSystem}    rotator
+${component}    exitControl
+${timeout}    30s
+
+*** Test Cases ***
+Create Commander Session
+    [Documentation]    Connect to the SAL host.
+    [Tags]    smoke
+    Comment    Connect to host.
+    Open Connection    host=${Host}    alias=Commander    timeout=${timeout}    prompt=${Prompt}
+    Comment    Login.
+    Log    ${ContInt}
+    Run Keyword If    "${ContInt}"=="false"    Login    ${UserName}    ${PassWord}
+    Run Keyword If    "${ContInt}"=="true"    Login With Public Key    ${UserName}    keyfile=${PassWord}
+    Directory Should Exist    ${SALInstall}
+    Directory Should Exist    ${SALHome}
+    Directory Should Exist    ${SALWorkDir}/${subSystem}
+
+Create Controller Session
+    [Documentation]    Connect to the SAL host.
+    [Tags]    smoke
+    Comment    Connect to host.
+    Open Connection    host=${Host}    alias=Controller    timeout=${timeout}    prompt=${Prompt}
+    Comment    Login.
+    Log    ${ContInt}
+    Run Keyword If    "${ContInt}"=="false"    Login    ${UserName}    ${PassWord}
+    Run Keyword If    "${ContInt}"=="true"    Login With Public Key    ${UserName}    keyfile=${PassWord}
+    Directory Should Exist    ${SALInstall}
+    Directory Should Exist    ${SALHome}
+    Directory Should Exist    ${SALWorkDir}/${subSystem}
+
+Verify Component Commander and Controller
+    [Tags]    smoke
+    File Should Exist    ${SALWorkDir}/${subSystem}/cpp/src/sacpp_${subSystem}_${component}_commander
+    File Should Exist    ${SALWorkDir}/${subSystem}/cpp/src/sacpp_${subSystem}_${component}_controller
+
+Start Commander - Verify Missing Inputs Error
+    [Tags]    functional
+    Switch Connection    Commander
+    Comment    Move to working directory.
+    Write    cd ${SALWorkDir}/${subSystem}/cpp/src
+    Comment    Start Commander.
+    ${input}=    Write    ./sacpp_${subSystem}_${component}_commander 
+    ${output}=    Read Until Prompt
+    Log    ${output}
+    Should Contain    ${output}   Usage :  input parameters...
+    Should Contain    ${output}   string    state;
+
+Start Commander - Verify Timeout without Controller
+    [Tags]    functional
+    Switch Connection    Commander
+    Comment    Move to working directory.
+    Write    cd ${SALWorkDir}/${subSystem}/cpp/src
+    Comment    Start Commander.
+    ${input}=    Write    ./sacpp_${subSystem}_${component}_commander true
+    ${output}=    Read Until Prompt
+    Log    ${output}
+    ${CmdComplete}=    Get Line    ${output}    -2
+    Should Match Regexp    ${CmdComplete}    (=== \\[waitForCompletion_${component}\\] command )[0-9]+( timed out :)
+
+Start Controller
+    [Tags]    functional
+    Switch Connection    Controller
+    Comment    Move to working directory.
+    Write    cd ${SALWorkDir}/${subSystem}/cpp/src
+    Comment    Start Controller.
+    ${input}=    Write    ./sacpp_${subSystem}_${component}_controller
+    ${output}=    Read Until    controller ready
+    Log    ${output}
+    Should Contain    ${output}    ${subSystem}_${component} controller ready
+
+Start Commander
+    [Tags]    functional
+    Switch Connection    Commander
+    Comment    Move to working directory.
+    Write    cd ${SALWorkDir}/${subSystem}/cpp/src
+    Comment    Start Commander.
+    ${input}=    Write    ./sacpp_${subSystem}_${component}_commander true
+    ${output}=    Read Until Prompt
+    Log    ${output}
+    Should Contain X Times    ${output}    === [issueCommand_${component}] writing a command containing :    1
+    Should Contain X Times    ${output}    device :    1
+    Should Contain X Times    ${output}    property :    1
+    Should Contain X Times    ${output}    action :    1
+    Should Contain X Times    ${output}    value :    1
+    Should Contain X Times    ${output}     : true    1
+    ${CmdComplete}=    Get Line    ${output}    -2
+    Should Match Regexp    ${CmdComplete}    (=== \\[waitForCompletion_${component}\\] command )[0-9]+( completed ok :)
+
+Read Controller
+    [Tags]    functional
+    Switch Connection    Controller
+    ${output}=    Read Until    result \ \ : Done : OK
+    Log    ${output}
+    Should Contain    ${output}    === command exitControl received =
+    Should Contain    ${output}    device :
+    Should Contain    ${output}    property :
+    Should Contain    ${output}    action : 
+    Should Contain    ${output}    value : 
+    Should Contain    ${output}    state : true
+    Should Contain    ${output}    ack      : 301
+    Should Contain    ${output}    result   : Ack : OK
+    Should Contain    ${output}    ack      : 303
+    Should Contain    ${output}    result   : Done : OK
+    Should Contain X Times    ${output}    seqNum \ \ :    2
+    Should Contain X Times    ${output}    error \ \ \ : 0    2
+    Should Contain X Times    ${output}    === [ackCommand_exitControl] acknowledging a command with :    2
---
+++ scripts/CppState.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+#  Shellscript to create test suites for the C++
+#  Commander/Controllers pairs.
+#
+#  author: Rob Bovill
+#  email:  rbovill@lsst.org
+
+
+#  Define variables to be used in script
+workDir=$HOME/trunk/robotframework_SAL/CPP/StateMachine
+arg=${1-all}
+arg="$(echo ${arg} |tr 'A-Z' 'a-z')"
+device=$EMPTY
+property=$EMPTY
+action=$EMPTY
+value=$EMPTY
+declare -a subSystemArray=(camera dome dm hexapod m1m3 m2ms mtmount rotator scheduler tcs)
+declare -a statesArray=(enable disable abort enterControl exitControl standby start stop)
+
+#  FUNCTIONS
+# Get the subsystem variable in the correct format.
+function getSubSystem() {
+	if [ "$1" == "mtmount" ]; then
+		echo MTMount
+	else
+		echo $1
+	fi
+}
+
+function clearTestSuite() {
+    if [ -f $testSuite ]; then
+        echo $testSuite exists.  Deleting it before creating a new one.
+        rm -rf $testSuite
+    fi
+}
+
+function createSettings() {
+    echo "*** Settings ***" >> $testSuite
+    echo "Documentation    ${subSystemUp} State Machine tests." >> $testSuite
+    echo "Suite Setup    Log Many    \${Host}    \${subSystem}    \${timeout}" >> $testSuite
+    echo "Suite Teardown    Close All Connections" >> $testSuite
+    echo "Library    SSHLibrary" >> $testSuite
+    echo "Library    String" >> $testSuite
+    echo "Resource    ../../Global_Vars.robot" >> $testSuite
+	echo "" >> $testSuite
+}
+
+function createVariables() {
+    echo "*** Variables ***" >> $testSuite
+    echo "\${subSystem}    $subSystem" >> $testSuite
+    echo "\${component}    $state" >> $testSuite
+    echo "\${timeout}    30s" >> $testSuite
+    echo "" >> $testSuite
+}
+
+function createCommanderSession() {
+    echo "Create Commander Session" >> $testSuite
+    echo "    [Documentation]    Connect to the SAL host." >> $testSuite
+    echo "    [Tags]    smoke" >> $testSuite
+    echo "    Comment    Connect to host." >> $testSuite
+    echo "    Open Connection    host=\${Host}    alias=Commander    timeout=\${timeout}    prompt=\${Prompt}" >> $testSuite
+    echo "    Comment    Login." >> $testSuite
+    echo "    Log    \${ContInt}" >> $testSuite
+    echo "    Run Keyword If    \"\${ContInt}\"==\"false\"    Login    \${UserName}    \${PassWord}" >> $testSuite
+    echo "    Run Keyword If    \"\${ContInt}\"==\"true\"    Login With Public Key    \${UserName}    keyfile=\${PassWord}" >> $testSuite
+    echo "    Directory Should Exist    \${SALInstall}" >> $testSuite
+    echo "    Directory Should Exist    \${SALHome}" >> $testSuite
+    echo "    Directory Should Exist    \${SALWorkDir}/\${subSystem}" >> $testSuite
+    echo "" >> $testSuite
+}
+
+function createControllerSession() {
+    echo "Create Controller Session" >> $testSuite
+    echo "    [Documentation]    Connect to the SAL host." >> $testSuite
+    echo "    [Tags]    smoke" >> $testSuite
+    echo "    Comment    Connect to host." >> $testSuite
+    echo "    Open Connection    host=\${Host}    alias=Controller    timeout=\${timeout}    prompt=\${Prompt}" >> $testSuite
+    echo "    Comment    Login." >> $testSuite
+    echo "    Log    \${ContInt}" >> $testSuite
+    echo "    Run Keyword If    \"\${ContInt}\"==\"false\"    Login    \${UserName}    \${PassWord}" >> $testSuite
+    echo "    Run Keyword If    \"\${ContInt}\"==\"true\"    Login With Public Key    \${UserName}    keyfile=\${PassWord}" >> $testSuite
+    echo "    Directory Should Exist    \${SALInstall}" >> $testSuite
+    echo "    Directory Should Exist    \${SALHome}" >> $testSuite
+    echo "    Directory Should Exist    \${SALWorkDir}/\${subSystem}" >> $testSuite
+    echo "" >> $testSuite
+}
+
+function verifyCompCommanderController() {
+    echo "Verify Component Commander and Controller" >> $testSuite
+    echo "    [Tags]    smoke" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/cpp/src/sacpp_\${subSystem}_\${component}_commander" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/cpp/src/sacpp_\${subSystem}_\${component}_controller" >> $testSuite
+    echo "" >> $testSuite
+}
+
+function startCommanderInputs() {
+    parameter=$EMPTY
+    echo "Start Commander - Verify Missing Inputs Error" >> $testSuite
+    echo "    [Tags]    functional" >> $testSuite
+    echo "    Switch Connection    Commander" >> $testSuite
+    echo "    Comment    Move to working directory." >> $testSuite
+    echo "    Write    cd \${SALWorkDir}/\${subSystem}/cpp/src" >> $testSuite
+    echo "    Comment    Start Commander." >> $testSuite
+    echo "    \${input}=    Write    ./sacpp_\${subSystem}_\${component}_commander $parameter" >> $testSuite
+    echo "    \${output}=    Read Until Prompt" >> $testSuite
+    echo "    Log    \${output}" >> $testSuite
+    echo "    Should Contain    \${output}   Usage :  input parameters..." >> $testSuite
+    echo "    Should Contain    \${output}   string    $parameterType;" >> $testSuite
+    echo "" >> $testSuite
+}
+
+function startCommanderTimeout() {
+	echo "Start Commander - Verify Timeout without Controller" >> $testSuite
+    echo "    [Tags]    functional" >> $testSuite
+    echo "    Switch Connection    Commander" >> $testSuite
+    echo "    Comment    Move to working directory." >> $testSuite
+    echo "    Write    cd \${SALWorkDir}/\${subSystem}/cpp/src" >> $testSuite
+    echo "    Comment    Start Commander." >> $testSuite
+    echo "    \${input}=    Write    ./sacpp_\${subSystem}_\${component}_commander true" >> $testSuite
+    echo "    \${output}=    Read Until Prompt" >> $testSuite
+    echo "    Log    \${output}" >> $testSuite
+	echo "    \${CmdComplete}=    Get Line    \${output}    -2" >>$testSuite
+    echo "    Should Match Regexp    \${CmdComplete}    (=== \\\[waitForCompletion_\${component}\\\] command )[0-9]+( timed out :)" >>$testSuite
+    echo "" >> $testSuite
+}
+
+function startController() {
+    echo "Start Controller" >> $testSuite
+    echo "    [Tags]    functional" >> $testSuite
+    echo "    Switch Connection    Controller" >> $testSuite
+    echo "    Comment    Move to working directory." >> $testSuite
+    echo "    Write    cd \${SALWorkDir}/\${subSystem}/cpp/src" >> $testSuite
+    echo "    Comment    Start Controller." >> $testSuite
+    echo "    \${input}=    Write    ./sacpp_\${subSystem}_\${component}_controller" >> $testSuite
+    echo "    \${output}=    Read Until    controller ready" >> $testSuite
+    echo "    Log    \${output}" >> $testSuite
+    echo "    Should Contain    \${output}    \${subSystem}_\${component} controller ready" >> $testSuite
+    echo "" >> $testSuite
+}
+
+function startCommander() {
+	i=0
+	n=0
+	device=$1
+	property=$2
+    echo "Start Commander" >> $testSuite
+    echo "    [Tags]    functional" >> $testSuite
+    echo "    Switch Connection    Commander" >> $testSuite
+    echo "    Comment    Move to working directory." >> $testSuite
+    echo "    Write    cd \${SALWorkDir}/\${subSystem}/cpp/src" >> $testSuite
+    echo "    Comment    Start Commander." >> $testSuite
+    echo "    \${input}=    Write    ./sacpp_\${subSystem}_\${component}_commander true" >> $testSuite
+    echo "    \${output}=    Read Until Prompt" >> $testSuite
+    echo "    Log    \${output}" >> $testSuite
+    echo "    Should Contain X Times    \${output}    === [issueCommand_\${component}] writing a command containing :    1" >> $testSuite
+    echo "    Should Contain X Times    \${output}    device :    1" >> $testSuite    #$device TSS-861
+    echo "    Should Contain X Times    \${output}    property :    1" >> $testSuite    #$property TSS-861
+    echo "    Should Contain X Times    \${output}    action :    1" >> $testSuite    #$action TSS-861
+    echo "    Should Contain X Times    \${output}    value :    1" >> $testSuite    #$value TSS-861
+    echo "    Should Contain X Times    \${output}    $parameter : true    1" >>$testSuite
+	echo "    \${CmdComplete}=    Get Line    \${output}    -2" >>$testSuite
+    echo "    Should Match Regexp    \${CmdComplete}    (=== \\\[waitForCompletion_\${component}\\\] command )[0-9]+( completed ok :)" >>$testSuite
+    echo "" >> $testSuite
+}
+
+function readController() {
+    echo "Read Controller" >> $testSuite
+    echo "    [Tags]    functional" >> $testSuite
+    echo "    Switch Connection    Controller" >> $testSuite
+    echo "    \${output}=    Read Until    result \ \ : Done : OK" >>$testSuite
+    echo "    Log    \${output}" >> $testSuite
+    echo "    Should Contain    \${output}    === command $state received =" >>$testSuite
+    echo "    Should Contain    \${output}    device :" >>$testSuite
+    echo "    Should Contain    \${output}    property :" >>$testSuite
+    echo "    Should Contain    \${output}    action : " >>$testSuite
+    echo "    Should Contain    \${output}    value : " >>$testSuite
+    echo "    Should Contain    \${output}    $parameterType : true" >>$testSuite
+    echo "    Should Contain    \${output}    ack      : 301" >>$testSuite
+    echo "    Should Contain    \${output}    result   : Ack : OK" >>$testSuite
+    echo "    Should Contain    \${output}    ack      : 303" >>$testSuite
+    echo "    Should Contain    \${output}    result   : Done : OK" >>$testSuite
+    echo "    Should Contain X Times    \${output}    seqNum \ \ :    2" >>$testSuite
+    echo "    Should Contain X Times    \${output}    error \ \ \ : 0    2" >> $testSuite
+	echo "    Should Contain X Times    \${output}    === [ackCommand_${state}] acknowledging a command with :    2" >> $testSuite
+}
+
+function createTestSuite() {
+	subSystem=$1
+	stateIndex=1
+	if [ "$subSystem" == "m1m3" ]; then
+		subSystemUp="M1M3"
+	elif [ "$subSystem" == "m2ms" ]; then
+		subSystemUp="M2MS"
+	elif [ "$subSystem" == "tcs" ]; then
+		subSystemUp="TCS"
+	elif [ "$subSystem" == "mtmount" ]; then
+		subSystemUp="MTMount"
+	else
+		subSystemUp="$(tr '[:lower:]' '[:upper:]' <<< ${subSystem:0:1})${subSystem:1}"
+	fi
+	for state in "${statesArray[@]}"; do
+		if [ "$state" == "start" ]; then
+			parameterType="configuration"
+		else
+			parameterType="state"
+		fi
+		property=$EMPTY
+		#  Define test suite name
+		testSuite=$workDir/${subSystemUp}_${state}.robot
+		
+		#  Check to see if the TestSuite exists then, if it does, delete it.
+		clearTestSuite
+		
+		#  Create test suite.
+		echo Creating $testSuite
+		createSettings
+		createVariables
+		echo "*** Test Cases ***" >> $testSuite
+		createCommanderSession
+		createControllerSession
+        verifyCompCommanderController
+		startCommanderInputs
+		# Create the Commander Timeout test case.
+		startCommanderTimeout
+		# Create the Start Controller test case.
+		startController
+		# Create the Start Commander test case.
+		startCommander $device $property
+		# Create the Read Controller test case.
+		readController $device $property
+		# Indicate completion of the test suite.
+		echo Done with test suite.
+    	# Move to next Topic.
+		(( stateIndex++ ))
+	done
+}
+
+
+#  MAIN
+if [ "$arg" == "all" ]; then
+	for i in "${subSystemArray[@]}"; do
+		subSystem=$(getSubSystem $i)
+		createTestSuite $subSystem
+	done
+	echo COMPLETED ALL test suites for ALL subsystems.
+elif [[ ${subSystemArray[*]} =~ $arg ]]; then
+	subSystem=$(getSubSystem $arg)
+	getTopics $subSystem
+	createTestSuite $subSystem
+	echo COMPLETED all test suites for the $arg.
+else
+	echo USAGE - Argument must be one of: ${subSystemArray[*]} OR all.
+fi
+
