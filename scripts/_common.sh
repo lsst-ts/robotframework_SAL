9a63becdbd9fa825456efc0b886755a8fb04b1b1
Rob Bovill <rbovill@lsst.org>
2017-02-28 12:30:21 -0700

    Added some formatting strings to a function in _common.sh.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -27,8 +27,10 @@ function clearTestSuites() {
 	subsystem_type=$(tr '[:lower:]' '[:upper:]' <<< ${1:0:1})${1:1} #Subsystem type is capitalized
 	language=$(echo $2 |tr [a-z] [A-Z]) #Programming language is fully capitalized
 	topic_type=$(tr '[:lower:]' '[:upper:]' <<< ${3:0:1})${3:1} #Topic type is capitalized 
+	echo "============================================================================================"
 	echo "Deleting: $(ls -1 $HOME/trunk/robotframework_SAL/$language/$topic_type/${subsystem_type}_*)"
     rm $HOME/trunk/robotframework_SAL/$language/$topic_type/${subsystem_type}_*
+	echo "============================================================================================"
 	echo ""
 }
 
