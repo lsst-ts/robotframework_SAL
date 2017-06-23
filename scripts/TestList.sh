4bf0ad1173c8d567d4c4f6fdfe42c951dd74e37c
Rob Bovill <rbovill@lsst.org>
2017-06-23 13:32:38 -0700

    Corrected TestList.sh to NOT run the Jenkins test list generators.

--- scripts/TestList.sh
+++ scripts/TestList.sh
@@ -1,4 +1,4 @@
-for SCRIPT in $HOME/trunk/robotframework_SAL/scripts/ArgumentLists/*
+for SCRIPT in $HOME/trunk/robotframework_SAL/scripts/ArgumentLists/TestList*
 do
 	if [ -f $SCRIPT -a -x $SCRIPT ]; then
 		$SCRIPT
