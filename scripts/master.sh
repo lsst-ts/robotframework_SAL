667df93d246e6ac142a977c7ef02d58076019a2b
Rob Bovill <rbovill@lsst.org>
2017-09-07 09:54:21 -0700

    Uncommented out the SalGen.sh script in master.sh.

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,2 +1,1 @@
-#./SalGen.sh* ; 
-./CppComCon.sh* ; ./CppEvents.sh* ; ./CppPubSub.sh* ; ./CppState.sh* ; ./JavaPubSub.sh* ; ./PythonComCon.sh* ; ./PythonEvents.sh* ; ./PythonPubSub.sh* ; ./PythonState.sh*
+./SalGen.sh* ; ./CppComCon.sh* ; ./CppEvents.sh* ; ./CppPubSub.sh* ; ./CppState.sh* ; ./JavaPubSub.sh* ; ./PythonComCon.sh* ; ./PythonEvents.sh* ; ./PythonPubSub.sh* ; ./PythonState.sh*
