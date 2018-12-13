be37c0396b0ac00deed8b12855be46e8fe09b6ce
Rob Bovill <rbovill@lsst.org>
2018-12-13 09:27:14 -0700

    Started updating JavaState.sh.

--- scripts/JavaState.sh
+++ scripts/JavaState.sh
@@ -19,27 +19,13 @@ declare -a statesArray=($(stateArray))
 
 #  Determine what tests to generate. Call _common.sh.generateTests()
 function main() {
-    arg=$(echo $1 |tr '[:upper:]' '[:lower:]')
+    arg=$1
 
-    # Get the XML definition file. This requires the CSC be capitalized properly. This in done in the _common.sh.getEntity() function.
-    subsystem=$(getEntity $arg)
-    file=($HOME/trunk/ts_xml/sal_interfaces/$subsystem/*_Telemetry.xml)
+    # Get the XML definition file.
+    file=($HOME/trunk/ts_xml/sal_interfaces/SALGenerics.xml)
 
     # Delete all test associated test suites first, to catch any removed topics.
     clearTestSuites $arg "JAVA" "StateMachine" || exit 1
-
-    #  CSCs should now explicitly define their generic commands. 
-    #  ... The tranisition process is ongoing, so some do this, and some do not.
-    #  ... As such, skip this step for the CSCs that are doing this.
-    declare -a array=($(stateMachineSkipped))
-    for item in "${array[@]}"; do 
-        if [[ "$item" == "$arg" ]]; then
-            echo "The $(capitializeSubsystem $subsystem) explicitly defines the generic commands and events"
-            echo "Skipping StateMachine tests."
-            echo ""
-            exit 0
-        fi
-    done
 
     # Now generate the test suites.
     createTestSuite $arg $file || exit 1
@@ -52,7 +38,7 @@ function createSettings() {
     echo "*** Settings ***" >> $testSuite
     echo "Documentation    $(capitializeSubsystem $subSystem)_${topic} communications tests." >> $testSuite
     echo "Force Tags    java    $skipped" >> $testSuite
-	echo "Suite Setup    Run Keywords    Log Many    \${Host}    \${subSystem}    \${component}    \${timeout}" >> $testSuite
+	echo "Suite Setup    Run Keywords    Log Many    \${Host}    \${subSystem}    \${timeout}" >> $testSuite
 	echo "...    AND    Create Session    Commander    AND    Create Session    Controller" >> $testSuite
     echo "Suite Teardown    Close All Connections" >> $testSuite
     echo "Library    SSHLibrary" >> $testSuite
@@ -63,33 +49,34 @@ function createSettings() {
 }
 
 function createVariables() {
-	local subSystem=$(getEntity $1)
+	local subSystem=$1
     echo "*** Variables ***" >> $testSuite
     echo "\${subSystem}    $subSystem" >> $testSuite
-    echo "\${component}    $state" >> $testSuite
     echo "\${timeout}    60s" >> $testSuite
     echo "" >> $testSuite
 }
 
 function verifyCompCommanderController() {
-    echo "Verify Component Commander and Controller" >> $testSuite
+    state=$1
+    echo "Verify $state Component Commander and Controller" >> $testSuite
     echo "    [Tags]    smoke" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}Commander_\${component}Test.java" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}Controller_\${component}Test.java" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Commander_\${component}Test.java" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Controller_\${component}Test.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}Commander_${state}Test.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}Controller_${state}Test.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Commander_${state}Test.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Controller_${state}Test.java" >> $testSuite
     echo "" >> $testSuite
 }
 
 function startCommanderInputs() {
+	state=$1
     parameter=$EMPTY
-    echo "Start Commander - Verify Missing Inputs Error" >> $testSuite
+    echo "Start $state Commander - Verify Missing Inputs Error" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Commander" >> $testSuite
     echo "    Comment    Move to working directory." >> $testSuite
     echo "    Write    cd \${SALWorkDir}/\${subSystem}/cpp/src" >> $testSuite
     echo "    Comment    Start Commander." >> $testSuite
-    echo "    \${input}=    Write    ./sacpp_\${subSystem}_\${component}_commander $parameter" >> $testSuite
+    echo "    \${input}=    Write    ./sacpp_\${subSystem}_${state}_commander $parameter" >> $testSuite
     echo "    \${output}=    Read Until Prompt" >> $testSuite
     echo "    Log    \${output}" >> $testSuite
     echo "    Should Contain    \${output}    Usage : \\ input parameters..." >> $testSuite
@@ -98,28 +85,30 @@ function startCommanderInputs() {
 }
 
 function startCommanderTimeout() {
-	echo "Start Commander - Verify Timeout without Controller" >> $testSuite
+	state=$1
+	echo "Start $state Commander - Verify Timeout without Controller" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Commander" >> $testSuite
     echo "    Comment    Move to working directory." >> $testSuite
     echo "    Write    cd \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}" >> $testSuite
     echo "    Comment    Run the Commander test." >> $testSuite
-    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Commander_\${component}Test test" >> $testSuite
+    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Commander_${state}Test test" >> $testSuite
     echo "    \${output}=    Read Until Prompt" >> $testSuite
     echo "    Log    \${output}" >> $testSuite
 	echo "    \${CmdComplete}=    Get Line    \${output}    -16" >>$testSuite
-    echo "    Should Match Regexp    \${CmdComplete}    (=== \\\[waitForCompletion_\${component}\\\] command )([0-9]+)( timed out)" >>$testSuite
+    echo "    Should Match Regexp    \${CmdComplete}    (=== \\\[waitForCompletion_${state}\\\] command )([0-9]+)( timed out)" >>$testSuite
     echo "" >> $testSuite
 }
 
 function startController() {
-    echo "Start Controller" >> $testSuite
+	state=$1
+    echo "Start $state Controller" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Controller" >> $testSuite
     echo "    Comment    Move to working directory." >> $testSuite
     echo "    Write    cd \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}" >> $testSuite
     echo "    Comment    Start the Controller test." >> $testSuite
-    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Controller_\${component}Test test" >> $testSuite
+    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Controller_${state}Test test" >> $testSuite
     #echo "    \${output}=    Read Until    Scanning for projects..." >> $testSuite
     echo "" >> $testSuite
 }
@@ -127,30 +116,32 @@ function startController() {
 function startCommander() {
 	i=0
 	n=0
-	device=$1
-	property=$2
-    echo "Start Commander" >> $testSuite
+	state=$1
+	device=$2
+	property=$3
+    echo "Start $state Commander" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Commander" >> $testSuite
     echo "    Comment    Move to working directory." >> $testSuite
     echo "    Write    cd \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}" >> $testSuite
     echo "    Comment    Run the Commander test." >> $testSuite
-    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Commander_\${component}Test test" >> $testSuite
+    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Commander_${state}Test test" >> $testSuite
     echo "    \${output}=    Read Until Prompt" >> $testSuite
     echo "    Log    \${output}" >> $testSuite
-    echo "    Should Contain X Times    \${output}    === [issueCommand] \${component} writing a command containing :    1" >> $testSuite
+    echo "    Should Contain X Times    \${output}    === [issueCommand] ${state} writing a command containing :    1" >> $testSuite
 	echo "    \${CmdComplete}=    Get Line    \${output}    -15" >>$testSuite
-    echo "    Should Match Regexp    \${CmdComplete}    (=== \\\[waitForCompletion_\${component}\\\] command )[0-9]+( completed ok)" >>$testSuite
+    echo "    Should Match Regexp    \${CmdComplete}    (=== \\\[waitForCompletion_${state}\\\] command )[0-9]+( completed ok)" >>$testSuite
     echo "" >> $testSuite
 }
 
 function readController() {
-    echo "Read Controller" >> $testSuite
+	state=$1
+    echo "Read $state Controller" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Controller" >> $testSuite
     echo "    \${output}=    Read Until Prompt" >>$testSuite
     echo "    Log    \${output}" >> $testSuite
-    echo "    Should Contain    \${output}    \${subSystem}_\${component} controller ready" >> $testSuite
+    echo "    Should Contain    \${output}    \${subSystem}_${state} controller ready" >> $testSuite
     echo "    Should Contain    \${output}    ack      : 301" >>$testSuite
     echo "    Should Contain    \${output}    result   : Ack : OK" >>$testSuite
     echo "    Should Contain    \${output}    ack      : 303" >>$testSuite
@@ -158,6 +149,7 @@ function readController() {
     echo "    Should Contain X Times    \${output}    seqNum \ \ :    2" >>$testSuite
     echo "    Should Contain X Times    \${output}    error \ \ \ : 0    2" >> $testSuite
 	echo "    Should Contain X Times    \${output}    === [ackCommand_${state}] acknowledging a command with :    2" >> $testSuite
+    echo "" >> $testSuite
 }
 
 function createTestSuite() {
@@ -165,36 +157,39 @@ function createTestSuite() {
     messageType="state"
 	stateIndex=1
 
+	#  Define test suite name
+	testSuite=$workDir/$(capitializeSubsystem $subSystem)_StateMachine.robot
+
+	# Check if CSC uses the Generic topics (most do, but a few do not).
+    # ... If not, skip this CSC.
+    output=$( xml sel -t -m "//SALSubsystems/Subsystem/Name[text()='${subSystem}']/../Generics" -v . -n $HOME/trunk/ts_xml/sal_interfaces/SALSubsystems.xml )
+    if [ "$output" == "no" ]; then
+        echo "The $subSystem CSC does not use the Generic topics. Exiting."; exit 0
+    fi 
+
     # Generate the test suite for each topic.
-    echo Generating:
-	for state in "${statesArray[@]}"; do
+    echo Generating $testSuite:
+	createSettings $subSystem
+	createVariables $subSystem
+	echo "*** Test Cases ***" >> $testSuite
+	for state in "start" "enable" "disable" "standby" "exitControl"; do
 		if [ "$state" == "start" ]; then
-			parameterType="configuration"
+			parameterType="settingsToApply"
 		else
-			parameterType="state"
+			parameterType="value"
 		fi
 		property=$EMPTY
-		#  Define test suite name
-		testSuite=$workDir/$(capitializeSubsystem $subSystem)_${state}.robot
 		
         #  Check if test suite should be skipped.
         skipped=$(checkIfSkipped $subSystem $topic $messageType)
 
-		#  Create test suite.
-		Creating $testSuite
-		createSettings $subSystem
-		createVariables $subSystem
-		echo "*** Test Cases ***" >> $testSuite
-        verifyCompCommanderController
-		#startCommanderInputs
-		# Create the Commander Timeout test case.
-		startCommanderTimeout
+        verifyCompCommanderController $state
 		# Create the Start Controller test case.
-		startController
+		startController $state
 		# Create the Start Commander test case.
-		startCommander $device $property
+		startCommander $state $device $property
 		# Create the Read Controller test case.
-		readController $device $property
+		readController $state $device $property
     	# Move to next Topic.
 		(( stateIndex++ ))
 	done
