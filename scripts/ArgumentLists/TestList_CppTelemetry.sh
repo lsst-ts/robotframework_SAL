84918b68adba774d43158cb73e93207ee29742c7
Rob Bovill <rbovill@lsst.org>
2019-04-02 18:54:58 +0000

    Added Combined testing to ArgumentLists/TestList_CppTelemetry.sh.

--- scripts/ArgumentLists/TestList_CppTelemetry.sh
+++ scripts/ArgumentLists/TestList_CppTelemetry.sh
@@ -27,3 +27,27 @@ echo "# List of test suites" >> $argfile
 
 # Find test suites and add them to the argument file.
 ls -1 $HOME/trunk/robotframework_SAL/CPP/Telemetry |sed 's/^/CPP\/Telemetry\//' >> $argfile
+
+# Define Combined interface testig filename
+argfile=$HOME/trunk/robotframework_SAL/CombinedCppTelemetry_Tests.list
+
+# Find and remove old argument file.
+ls $argfile 1>/dev/null
+if [ $? -eq 0 ]; then
+        echo "Deleteing $argfile"
+        rm $argfile
+else
+        echo "$argfile does not exist.  Continuing."
+fi
+
+# Setup argument file
+echo "###  Test Suite list ###" >> $argfile
+echo "#  Change the title of the test results" >> $argfile
+echo "--name CombinedCppTelemetry" >> $argfile
+echo "--output CombinedCppTelemetry" >> $argfile
+echo "" >> $argfile
+echo "# List of test suites" >> $argfile
+
+# Find test suites and add them to the argument file.
+ls -1 $HOME/trunk/robotframework_SAL/Combined/CPP |sed 's/^/CPP\/Telemetry\//' >> $argfile
+
