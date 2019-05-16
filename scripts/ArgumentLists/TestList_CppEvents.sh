859e72e8c4dfe54c94cc53aae92613c252292058
Rob Bovill <rbovill@lsst.org>
2019-05-16 00:12:44 +0000

    Added the Combined/CPP/Events test suites to ArgumentLists/TestList_CppEvents.sh.

--- scripts/ArgumentLists/TestList_CppEvents.sh
+++ scripts/ArgumentLists/TestList_CppEvents.sh
@@ -27,3 +27,26 @@ echo "# List of test suites" >> $argfile
 
 # Find test suites and add them to the argument file.
 ls -1 $HOME/trunk/robotframework_SAL/CPP/Events |sed 's/^/CPP\/Events\//' >> $argfile
+
+# Define Combined interface testig filename
+argfile=$HOME/trunk/robotframework_SAL/CombinedCppEvents_Tests.list
+
+# Find and remove old argument file.
+ls $argfile 1>/dev/null
+if [ $? -eq 0 ]; then
+        echo "Deleteing $argfile"
+        rm $argfile
+else
+        echo "$argfile does not exist.  Continuing."
+fi
+
+# Setup argument file
+echo "###  Test Suite list ###" >> $argfile
+echo "#  Change the title of the test results" >> $argfile
+echo "--name CombinedCppEvents" >> $argfile
+echo "--output CombinedCppEvents" >> $argfile
+echo "" >> $argfile
+echo "# List of test suites" >> $argfile
+
+# Find test suites and add them to the argument file.
+ls -1 $HOME/trunk/robotframework_SAL/Combined/CPP/Events |sed 's/^/Combined\/CPP\/Events\//' >> $argfile
