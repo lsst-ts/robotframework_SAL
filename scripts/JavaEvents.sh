ae8bc7c58c66f055399153bfeed7645779ceda82
Andrew Heyer <aheyer@lsst.org>
2019-08-09 15:02:04 -0700

    conitnuing on java combined script

--- scripts/JavaEvents.sh
+++ scripts/JavaEvents.sh
@@ -10,6 +10,7 @@ source $ROBOTFRAMEWORK_SAL_DIR/scripts/_common.sh
 
 #  Define variables to be used in script
 workDir=$ROBOTFRAMEWORK_SAL_DIR/JAVA/Events
+workDirCombined=$ROBOTFRAMEWORK_SAL_DIR/Combined/JAVA/Events
 device=$EMPTY
 property=$EMPTY
 action=$EMPTY
@@ -17,6 +18,7 @@ value=$EMPTY
 declare -a topicsArray=($EMPTY)
 declare -a parametersArray=($EMPTY)
 declare -a argumentsArray=($EMPTY)
+declare -a generic_events=($(xml sel -t -m "//SALObjects/SALEventSet/SALEvent/EFDB_Topic" -v . -n $TS_XML_DIR/sal_interfaces/SALGenerics.xml |cut -d"_" -f 3 ))
 
 #  Determine what tests to generate. Call _common.sh.generateTests()
 function main() {
@@ -87,20 +89,30 @@ function getParameterCount() {
 
 function createSettings() {
     local subSystem=$1
+    local topic=$(tr '[:lower:]' '[:upper:]' <<< ${2:0:1})${2:1}
+    local testSuite=$3
     echo "*** Settings ***" >> $testSuite
     echo "Documentation    $(capitializeSubsystem $subSystem)_${topic} communications tests." >> $testSuite
     echo "Force Tags    java    $skipped" >> $testSuite
-	echo "Suite Setup    Run Keywords    Log Many    \${Host}    \${subSystem}    \${component}    \${timeout}" >> $testSuite
-	echo "...    AND    Create Session    Sender    AND    Create Session    Logger" >> $testSuite
+	echo "Suite Setup    Log Many    \${Host}    \${subSystem}    \${component}    \${timeout}" >> $testSuite
     echo "Suite Teardown    Close All Connections" >> $testSuite
-    echo "Library    SSHLibrary" >> $testSuite
-    echo "Resource    ../../Global_Vars.robot" >> $testSuite
-    echo "Resource    ../../common.robot" >> $testSuite
+    echo "Library    OperatingSystem" >> $testSuite
+    echo "Library    Collections" >> $testSuite
+    echo "Library    Process" >> $testSuite
+    echo "Library    String" >> $testSuite
+    echo "Resource    \${EXECDIR}\${/}Global_Vars.robot" >> $testSuite
 	echo "" >> $testSuite
 }
 
 function createVariables() {
-	subSystem=$(getEntity $1)
+	local subSystem=$1
+    local testSuite=$2
+    local topic=$3
+    if [ "$topic" == "all" ]; then
+        timeout="45s"
+    else
+        timeout="3s"
+    fi
     echo "*** Variables ***" >> $testSuite
     echo "\${subSystem}    $subSystem" >> $testSuite
     echo "\${component}    $topic" >> $testSuite
@@ -109,12 +121,13 @@ function createVariables() {
 }
 
 function verifyCompSenderLogger() {
+    local testSuite=$1
     echo "Verify Component Sender and Logger" >> $testSuite
     echo "    [Tags]    smoke" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}Event_\${component}Test.java" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}EventLogger_\${component}Test.java" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Event_\${component}Test.java" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}EventLogger_\${component}Test.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}Event_\${component}.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}/java/src/\${subSystem}EventLogger_\${component}.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Event_\${component}.java" >> $testSuite
+    echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}EventLogger_\${component}.java" >> $testSuite
     echo "" >> $testSuite
 }
 
@@ -134,28 +147,48 @@ function startSenderInputs() {
 }
 
 function startLogger() {
+    # echo "Start Logger" >> $testSuite
+    # echo "    [Tags]    functional" >> $testSuite
+    # echo "    Switch Connection    Logger" >> $testSuite
+    # echo "    Comment    Move to working directory." >> $testSuite
+    # echo "    Write    cd \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}" >> $testSuite
+    # echo "    Comment    Start the EventLogger test." >> $testSuite
+    # echo "    \${input}=    Write    mvn -Dtest=\${subSystem}EventLogger_\${component}Test test" >> $testSuite
+    # #echo "    \${output}=    Read Until    Scanning for projects..." >> $testSuite
+    # echo "" >> $testSuite
+
+    local subSystem=$1
+    local testSuite=$2
+    local topic=$3
+
     echo "Start Logger" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
-    echo "    Switch Connection    Logger" >> $testSuite
-    echo "    Comment    Move to working directory." >> $testSuite
-    echo "    Write    cd \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}" >> $testSuite
+    echo "    Comment    Start Logger." >> $testSuite
     echo "    Comment    Start the EventLogger test." >> $testSuite
-    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}EventLogger_\${component}Test test" >> $testSuite
-    #echo "    \${output}=    Read Until    Scanning for projects..." >> $testSuite
+    echo "    \${output}=    Start Process    mvn    -Dtest\=\${subSystem}EventLogger_all.java    test    cwd=\${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/    alias=logger    stdout=\${EXECDIR}\${/}stdout.txt    stderr=\${EXECDIR}\${/}stderr.txt" >> $testSuite    
+    echo "    Log    \${output}" >> $testSuite
+    echo "    Should Contain    \"\${output}\"    \"1\"" >> $testSuite
+    echo "    Wait Until Keyword Succeeds    200    1s    File Should Not Be Empty    \${EXECDIR}\${/}stdout.txt" >> $testSuite
+    echo "    :FOR    \${i} IN RANGE    30" >> $testSuite
+    echo "    \\    \${output}=    Get File    \${EXECDIR}\${/}stdout.txt" >> $testSuite
+    echo "    \\    Exit For Loop If     '${subSystem} all loggers ready' in \$output" >> $testSuite
+    echo "    \\    Sleep    1s" >> testSuite
+    echo ""
+
+
     echo "" >> $testSuite
+
 }
 
 function startSender() {
-	i=0
-	device=$1
-	property=$2
+	local testSuite=$1
     echo "Start Sender" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Sender" >> $testSuite
     echo "    Comment    Move to working directory." >> $testSuite
     echo "    Write    cd \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}" >> $testSuite
     echo "    Comment    Run the Event test." >> $testSuite
-    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Event_\${component}Test test" >> $testSuite
+    echo "    \${input}=    Write    mvn -Dtest=\${subSystem}Event_all test" >> $testSuite
     echo "    \${output}=    Read Until Prompt" >> $testSuite
     echo "    Log    \${output}" >> $testSuite
     echo "    Should Contain X Times    \${output}    === [putSample logevent_${topic}] writing a message containing :    1" >> $testSuite
@@ -164,9 +197,7 @@ function startSender() {
 }
 
 function readLogger() {
-	i=0
-	device=$1
-	property=$2
+	local testSuite=$1
     echo "Read Logger" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Switch Connection    Logger" >> $testSuite
@@ -186,58 +217,85 @@ function createTestSuite() {
     getTopics $subSystem $file
 
     # Generate the test suite for each topic.
-    echo Generating:
-	for topic in "${topicsArray[@]}"; do
-		device=$EMPTY
-		property=$EMPTY
-		#  Define test suite name
-		testSuite=$workDir/$(capitializeSubsystem $subSystem)_${topic}.robot
+    echo ============== Generating Combined messaging test suite ==============
+    testSuiteCombined=$workDirCombined/$(capitializeSubsystem $subSystem)_$(tr '[:lower:]' '[:upper:]' <<< ${messageType:0:1})${messageType:1}.robot
+    echo $testSuiteCombined
+    createSettings $subSystem $messageType $testSuiteCombined
+    createVariables $subSystem $testSuiteCombined "all"
+    echo "*** Test Cases ***" >> $testSuiteCombined
+    verifyCompSenderLogger $testSuiteCombined
+    startLogger $subSystem $testSuiteCombined
+    startSender $subSystem $testSuiteCombined
+    readLogger $file $topicIndex $testSuiteCombined
+    echo ==== Combined test generation complete ====
+    echo ""
+ #    echo Generating:
+	# for topic in "${topicsArray[@]}"; do
+	# 	device=$EMPTY
+	# 	property=$EMPTY
+	# 	#  Define test suite name
+	# 	testSuite=$workDir/$(capitializeSubsystem $subSystem)_${topic}.robot
 		
-		#  Get EFDB_Topic elements
-		getTopicParameters $file $topicIndex
-		device=$( xml sel -t -m "//SALEventSet/SALEvent[$topicIndex]/Device" -v . -n $file )
-		property=$( xml sel -t -m "//SALEventSet/SALEvent[$topicIndex]/Property" -v . -n $file )
+	# 	#  Get EFDB_Topic elements
+	# 	getTopicParameters $file $topicIndex
+	# 	device=$( xml sel -t -m "//SALEventSet/SALEvent[$topicIndex]/Device" -v . -n $file )
+	# 	property=$( xml sel -t -m "//SALEventSet/SALEvent[$topicIndex]/Property" -v . -n $file )
 
-        #  Check if test suite should be skipped.
-        skipped=$(checkIfSkipped $subSystem $topic $messageType)
+ #        #  Check if test suite should be skipped.
+ #        skipped=$(checkIfSkipped $subSystem $topic $messageType)
 
-		#  Create test suite.
-		echo $testSuite
-		createSettings $subSystem
-		createVariables $subSystem
-		echo "*** Test Cases ***" >> $testSuite
-        verifyCompSenderLogger
-		#startSenderInputs
-		startLogger
+	# 	#  Create test suite.
+	# 	echo $testSuite
+	# 	createSettings $subSystem
+	# 	createVariables $subSystem
+	# 	echo "*** Test Cases ***" >> $testSuite
+ #        verifyCompSenderLogger
+	# 	#startSenderInputs
+	# 	startLogger
 
-		# Get the arguments to the sender.
-		unset argumentsArray
-		# Determine the parameter type and create a test value, accordingly.
-        #for parameter in "${parametersArray[@]}"; do
-            #parameterIndex=$(getParameterIndex $parameter)
-            #parameterType=$(getParameterType $file $topicIndex $parameterIndex)
-            #parameterCount=$(getParameterCount $file $topicIndex $parameterIndex)
-			#parameterIDLSize=$(getParameterIDLSize $subSystem $topicIndex $parameterIndex)
-			#echo $parameter $parameterIndex $parameterType $parameterCount $parameterIDLSize
-			#for i in $(seq 1 $parameterCount); do
-                #testValue=$(generateArgument "$parameterType" $parameterIDLSize)
-                #argumentsArray+=( $testValue )
-            #done
-		#done
-		# The Event priority is a required argument to ALL senders, but is not in the XML definitions.
-		# ... As such, manually add this argument as the first element in argumentsArray and parametersArray.
-		#parametersArray=("${parametersArray[@]}" "priority")
-		#priority=$(python random_value.py long)
-		#argumentsArray=("${argumentsArray[@]}" "$priority")
-		# Create the Start Sender test case.
-		startSender $device $property
-		# Create the Read Logger test case.
-		readLogger $device $property
-    	# Move to next Topic.
-		(( topicIndex++ ))
-	done
-	echo ""
+	# 	# Get the arguments to the sender.
+	# 	unset argumentsArray
+	# 	# Determine the parameter type and create a test value, accordingly.
+ #        #for parameter in "${parametersArray[@]}"; do
+ #            #parameterIndex=$(getParameterIndex $parameter)
+ #            #parameterType=$(getParameterType $file $topicIndex $parameterIndex)
+ #            #parameterCount=$(getParameterCount $file $topicIndex $parameterIndex)
+	# 		#parameterIDLSize=$(getParameterIDLSize $subSystem $topicIndex $parameterIndex)
+	# 		#echo $parameter $parameterIndex $parameterType $parameterCount $parameterIDLSize
+	# 		#for i in $(seq 1 $parameterCount); do
+ #                #testValue=$(generateArgument "$parameterType" $parameterIDLSize)
+ #                #argumentsArray+=( $testValue )
+ #            #done
+	# 	#done
+	# 	# The Event priority is a required argument to ALL senders, but is not in the XML definitions.
+	# 	# ... As such, manually add this argument as the first element in argumentsArray and parametersArray.
+	# 	#parametersArray=("${parametersArray[@]}" "priority")
+	# 	#priority=$(python random_value.py long)
+	# 	#argumentsArray=("${argumentsArray[@]}" "$priority")
+	# 	# Create the Start Sender test case.
+	# 	startSender $device $property
+	# 	# Create the Read Logger test case.
+	# 	readLogger $device $property
+ #    	# Move to next Topic.
+	# 	(( topicIndex++ ))
+	# done
+	# echo ""
 }
 
 #### Call the main() function ####
 main $1
+
+
+# Start Logger
+#     ${output}=    Start Process    mvn    -Dtest\=ATAOSEventLogger_all.java    test    cwd=/home/aheyer/tsrepos/ts_sal/test/maven/ATAOS_3.10.0/    alias=Logger     stdout=${EXECDIR}${/}stdout.txt    stderr=${EXECDIR}${/}stderr.txt
+#     Log    ${output}
+#     Should Contain    "${output}"    "1"
+#     Wait Until Keyword Succeeds    200    1s    File Should Not Be Empty    ${EXECDIR}${/}stdout.txt
+#     Log    Doing setup    console=${True}
+#     :FOR    ${i}    IN RANGE    999999
+#     \    
+#     \    ${output}=    Get File    ${EXECDIR}${/}stdout.txt
+#     \    Log    fdsafdsa setup    console=${True}
+#     \    Log    Doing sfdsafdsfp    console=${True}
+#     \    Exit For Loop If    '${subSystem} all loggers ready' in $output
+#     \    
