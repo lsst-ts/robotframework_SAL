e71039d43a4649bd44fedfed8f8eaafc59b5083e
rbovill <rbovill@lsst.org>
2019-09-11 12:59:26 -0700

    Many changes to JavaPubSub.sh to implement the new Java single file tests.

--- scripts/JavaPubSub.sh
+++ scripts/JavaPubSub.sh
@@ -42,7 +42,7 @@ function getTopics {
 	topicsArray=($output)
 }
 
-function getTopicItems {
+function getTopicParameters {
 	file=$1
 	index=$2
 	output=$( xml sel -t -m "//SALTelemetrySet/SALTelemetry[$index]/item/EFDB_Name" -v . -n $file )
@@ -57,11 +57,9 @@ function clearTestSuite {
 }
 
 function createSettings {
-
     local subSystem=$1
     local topic=$(tr '[:lower:]' '[:upper:]' <<< ${2:0:1})${2:1}
     local testSuite=$3
-
     echo "*** Settings ***" >> $testSuite
     echo "Documentation    $(capitializeSubsystem $subSystem)_${topic} communications tests." >> $testSuite
     echo "Force Tags    java    $skipped" >> $testSuite
@@ -76,24 +74,28 @@ function createSettings {
 }
 
 function createVariables {
-
     local subSystem=$1
     local testSuite=$2
     local topic=$3
-
-    subSystem=$(getEntity $1)
+    local subSystem=$(getEntity $1)
     echo "*** Variables ***" >> $testSuite
     echo "\${subSystem}    $subSystem" >> $testSuite
     echo "\${component}    $topic" >> $testSuite
-    echo "\${timeout}    30s" >> $testSuite
+    echo "\${timeout}    90s" >> $testSuite
     echo "" >> $testSuite
 }
 
 function verifyCompPubSub {
+    local testSuite=$1
     echo "Verify Component Publisher and Subscriber" >> $testSuite
     echo "    [Tags]    smoke" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}_\${component}/java/standalone/saj_\${subSystem}_\${component}_pub.jar" >> $testSuite
-    echo "    File Should Exist    \${SALWorkDir}/\${subSystem}_\${component}/java/standalone/saj_\${subSystem}_\${component}_sub.jar" >> $testSuite
+	if [ $topic ]; then
+    	echo "    File Should Exist    \${SALWorkDir}/\${subSystem}_\${component}/java/standalone/saj_\${subSystem}_\${component}_pub.jar" >> $testSuite
+    	echo "    File Should Exist    \${SALWorkDir}/\${subSystem}_\${component}/java/standalone/saj_\${subSystem}_\${component}_sub.jar" >> $testSuite
+    else
+        echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Publisher_all.java" >> $testSuite
+        echo "    File Should Exist    \${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/src/test/java/\${subSystem}Subscriber_all.java" >> $testSuite
+    fi
     echo "" >> $testSuite
 }
 
@@ -105,56 +107,72 @@ function startJavaCombinedSubscriberProcess {
     # This is necessary otherwise the robot program will close before any of the 
     # processes have time to complete and communicate with each other which is 
     # what these robot files are testing.
-
     local subSystem=$1
     local topic=$2
     local testSuite=$3
-
     echo "Start Subscriber" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
     echo "    Comment    Executing Combined Java Subscriber Program." >> $testSuite
-    echo "    \${subscriberOutput}=    Start Process    mvn    -Dtest\=\${subSystem}Subscriber_all.java    test    cwd=\${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/    alias=logger    stdout=\${EXECDIR}\${/}stdoutSubscriber.txt    stderr=\${EXECDIR}\${/}stderrSubscriber.txt" >> $testSuite    
+    echo "    \${subscriberOutput}=    Start Process    mvn    -Dtest\=\${subSystem}Subscriber_all.java    test    cwd=\${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/    alias=Subscriber    stdout=\${EXECDIR}\${/}stdoutSubscriber.txt    stderr=\${EXECDIR}\${/}stderrSubscriber.txt" >> $testSuite    
+	echo "    Log    \${subscriberOutput}" >> $testSuite
+    echo "    Should Contain    \"\${subscriberOutput}\"   \"1\"" >> $testSuite
     echo "    Wait Until Keyword Succeeds    30    1s    File Should Not Be Empty    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
+    echo "    Comment    Wait for Subscriber program to be ready." >> $testSuite
+    echo "    \${subscriberOutput}=    Get File    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
+    echo "    :FOR    \${i}    IN RANGE    30" >> $testSuite
+    echo "    \\    Exit For Loop If     '\${subSystem} all subscribers ready' in \$subscriberOutput" >> $testSuite
+    echo "    \\    \${subscriberOutput}=    Get File    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
+    echo "    \\    Sleep    3s" >> $testSuite
+	echo "    Log    \${subscriberOutput}" >> $testSuite
+    echo "    Should Contain    \${subscriberOutput}    ===== \${subSystem} all subscribers ready =====" >> $testSuite
     echo "" >> $testSuite
 }
 
 function startJavaCombinedPublisherProcess {
     # Wait for the Subscriber program to be ready. It will know by a specific text
     # that the program generates. 
-
     local subSystem=$1
     local topic=$2
     local testSuite=$3
-
     echo "Start Publisher" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
-    
-    echo "    Comment    Publisher program waiting for Subscriber program to be Ready." >> $testSuite
-    echo "    \${subscriberOutput}=    Get File    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
-    echo "    :FOR    \${i}    IN RANGE    30" >> $testSuite
-    echo "    \\    Exit For Loop If     '${subSystem} all subscribers ready' in \$subscriberOutput" >> $testSuite
-    echo "    \\    \${subscriberOutput}=    Get File    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
-    echo "    \\    Sleep    3s" >> $testSuite
-    
-    echo "    Comment    Executing Combined Java Sender Program." >> $testSuite
-    echo "    \${publisherOutput}=    Start Process    mvn    -Dtest\=\${subSystem}Publisher_all.java    test    cwd=\${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/    alias=sender    stdout=\${EXECDIR}\${/}stdoutPublisher.txt    stderr=\${EXECDIR}\${/}stderrPublisher.txt" >> $testSuite    
-    echo "    :FOR    \${i}    IN RANGE    30" >> $testSuite
-    echo "    \\    \${subscriberOutput}=    Get File    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
-    echo "    \\    Run Keyword If    'message received :200' in \$subscriberOutput    Set Test Variable    \${publisherCompletionTextFound}    \"TRUE\"" >> $testSuite
-    echo "    \\    Exit For Loop If     'BUILD SUCCESS' in \$subscriberOutput" >> $testSuite
-    echo "    \\    Sleep    3s" >> $testSuite
-    
-    echo "    Should Be True    \${publisherCompletionTextFound} == \"TRUE\"" >> $testSuite
+    echo "    Comment    Executing Combined Java Publisher Program." >> $testSuite
+    echo "    \${publisherOutput}=    Run Process    mvn    -Dtest\=\${subSystem}Publisher_all.java    test    cwd=\${SALWorkDir}/maven/\${subSystem}_\${SALVersion}/    alias=Publisher    stdout=\${EXECDIR}\${/}stdoutPublisher.txt    stderr=\${EXECDIR}\${/}stderrPublisher.txt" >> $testSuite    
+    echo "    Log    \${publisherOutput.stdout}" >> $testSuite
+    echo "    Should Contain    \${publisherOutput.stdout}    ===== \${subSystem} all publishers ready =====" >> $testSuite
+    echo "    Should Contain    \${publisherOutput.stdout}    [INFO] BUILD SUCCESS" >> $testSuite
+    #echo "    :FOR    \${i}    IN RANGE    30" >> $testSuite
+    #echo "    \\    \${subscriberOutput}=    Get File    \${EXECDIR}\${/}stdoutSubscriber.txt" >> $testSuite
+    #echo "    \\    Run Keyword If    'message received :200' in \$subscriberOutput    Set Test Variable    \${publisherCompletionTextFound}    \"TRUE\"" >> $testSuite
+    #echo "    \\    Exit For Loop If     'BUILD SUCCESS' in \$subscriberOutput" >> $testSuite
+    #echo "    \\    Sleep    3s" >> $testSuite
+    #echo "    Should Be True    \${publisherCompletionTextFound} == \"TRUE\"" >> $testSuite
+    echo "" >> $testSuite
 }
 
 function readSubscriber {
+    local testSuite=$1
     echo "Read Subscriber" >> $testSuite
     echo "    [Tags]    functional" >> $testSuite
-    echo "    Switch Connection    Subscriber" >> $testSuite
-    echo "    \${output}=    Read    delay=1s" >> $testSuite
-    echo "    Log    \${output}" >> $testSuite
-    echo "    Should Contain X Times    \${output}    [\${component} Subscriber] samples    5" >> $testSuite
-    echo "    Should Contain X Times    \${output}    [\${component} Subscriber] message received :    5" >> $testSuite
+    echo "    Switch Process    Subscriber" >> $testSuite
+	echo "    \${output}=    Wait For Process    Subscriber    timeout=\${timeout}    on_timeout=terminate" >> $testSuite
+    echo "    Log Many    \${output.stdout}    \${output.stderr}" >> $testSuite
+    echo "    Should Contain    \${output.stdout}    ===== $subSystem all subscribers ready =====" >> $testSuite
+    echo "    @{full_list}=    Split To Lines    \${output.stdout}    start=34" >> $testSuite
+	itemIndex=1
+    for item in "${topicsArray[@]}"; do
+		echo "    \${${item}_start}=    Get Index From List    \${full_list}    === ${subSystem}_${item} start of topic ===" >> $testSuite
+		echo "    \${${item}_end}=    Get Index From List    \${full_list}    === ${subSystem}_${item} end of topic ===" >> $testSuite
+		echo "    \${${item}_list}=    Get Slice From List    \${full_list}    start=\${${item}_start}    end=\${${item}_end + 1}" >> $testSuite
+		echo "    Log Many    \${${item}_list}" >> $testSuite
+		echo "    Should Contain    \${${item}_list}    === ${subSystem}_${item} start of topic ===" >> $testSuite
+		echo "    Should Contain    \${${item}_list}    === ${subSystem}_${item} end of topic ===" >> $testSuite
+		#echo "    Should Contain    \${${item}_list}    === [$item Subscriber] samples" >> $testSuite
+        #echo "    Should Contain    \${${item}_list}    === [$item Subscriber] message received :1" >> $testSuite
+		#getTopicParameters $file $itemIndex
+		#readSubscriber_params $file $item $itemIndex $testSuite
+		(( itemIndex++ ))
+	done
 }
 
 function createTestSuite {
@@ -174,8 +192,10 @@ function createTestSuite {
     createVariables $subSystem $testSuiteCombined "all"
 
     echo "*** Test Cases ***" >> $testSuiteCombined
+    verifyCompPubSub $testSuiteCombined
     startJavaCombinedSubscriberProcess $subSystem $messageType $testSuiteCombined
     startJavaCombinedPublisherProcess $subSystem $messageType $testSuiteCombined
+	readSubscriber $testSuiteCombined
 
  #    echo Generating:
 	# for topic in "${topicsArray[@]}"; do
