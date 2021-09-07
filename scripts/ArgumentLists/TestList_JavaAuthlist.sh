4a3a2606d552195fe51fa8405b0ba01fb674a532
Rob Bovill <rbovill@lsst.org>
2021-09-07 13:18:53 -0700

    Added the Java AuthList tests.
    Added the Java AuthList test-suite generator to GenerateTests.py.

--- scripts/ArgumentLists/TestList_JavaAuthlist.sh
+++ scripts/ArgumentLists/TestList_JavaAuthlist.sh
@@ -6,7 +6,7 @@
 #  email:  rbovill@lsst.org
 
 # Define filename
-argfile=$ROBOTFRAMEWORK_SAL_DIR/CppAuthList_Tests.list
+argfile=$ROBOTFRAMEWORK_SAL_DIR/JavaAuthList_Tests.list
 
 # Find and remove old argument file.
 ls $argfile 1>/dev/null
@@ -20,10 +20,10 @@ fi
 # Setup argument file
 echo "###  Test Suite list ###" >> $argfile
 echo "#  Change the title of the test results" >> $argfile
-echo "--name CppAuthList" >> $argfile
-echo "--output CppAuthList" >> $argfile
+echo "--name JavaAuthList" >> $argfile
+echo "--output JavaAuthList" >> $argfile
 echo "" >> $argfile
 echo "# List of test suites" >> $argfile
 
 # Find test suites and add them to the argument file.
-ls -1 $ROBOTFRAMEWORK_SAL_DIR/AuthList/CPP |sed 's/^/AuthList\/CPP\//' >> $argfile
+ls -1 $ROBOTFRAMEWORK_SAL_DIR/AuthList/JAVA |sed 's/^/AuthList\/JAVA\//' >> $argfile
