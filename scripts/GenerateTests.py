00efb634c513f24a871a510b129135b1861f57e6
rbovill <rbovill@lsst.org>
2024-05-06 17:23:23 -0700

    Updated GenerateTests.py to build either the DDS or Kafka messaging test suites.

--- scripts/GenerateTests.py
+++ scripts/GenerateTests.py
@@ -8,40 +8,42 @@ import argparse
 # This array defines the list of current CSCs.
 csc_array = ['ATAOS', 'ATBuilding', 'ATCamera', 'ATDome', 'ATDomeTrajectory', 'ATHeaderService', 'ATHexapod', 'ATMCS', 'ATMonochromator', 'ATOODS', 'ATPneumatics', 'ATPtg', 'ATSpectrograph', 'ATWhiteLight', 'CBP', 'CCCamera', 'CCHeaderService', 'CCOODS', 'DIMM', 'DREAM', 'DSM', 'EAS', 'Electrometer', 'ESS', 'FiberSpectrograph', 'GCHeaderService', 'GenericCamera', 'GIS', 'Guider', 'HVAC', 'LaserTracker', 'LEDProjetor', 'LinearStage', 'LOVE', 'MTAirCompressor', 'MTAOS', 'MTCamera', 'MTDome', 'MTDomeTrajectory', 'MTEEC', 'MTHeaderService', 'MTHexapod', 'MTM1M3', 'MTM1M3TS', 'MTM2', 'MTMount', 'MTOODS', 'MTPtg', 'MTRotator', 'MTVMS', 'OCPS', 'PMD', 'Scheduler', 'Script', 'ScriptQueue', 'SummitFacility', 'Test', 'TunableLaser', 'Watcher', 'WeatherForecast']
 
-def GenerateTests(csc, language):
+def GenerateTests(csc, language, dds):
     """Test Suite generator."""
     complete=True
     print("Starting test suite generation...\n")
-    if  csc == "all":
+    if dds:
+        print(f"This is a DDS build.")
+        path = "DDS_"
+    else:
+        print(f"This is a Kafka build.")
+        path = ""
+    if csc == "all":
         for csc in csc_array:
             if ('cpp' in language) or ('all' in language):
-                subprocess.check_call(["./CppPubSub.sh", csc])
-                subprocess.check_call(["./CppEvents.sh", csc])
-                subprocess.check_call(["./CppAuthlist.sh", csc])
-                #subprocess.check_call(["./CppState.sh", csc])
-                #subprocess.check_call(["./CppComCon.sh", csc])
+                subprocess.check_call([f"./{path}CppPubSub.sh", csc])
+                subprocess.check_call([f"./{path}CppEvents.sh", csc])
+                #subprocess.check_call([f"./{path}CppState.sh", csc])
+                #subprocess.check_call([f"./{path}CppComCon.sh", csc])
             if ('java' in language) or ('all' in language):
-                subprocess.check_call(["./JavaPubSub.sh", csc])
-                subprocess.check_call(["./JavaEvents.sh", csc])
-                subprocess.check_call(["./JavaAuthlist.sh", csc])
-                #subprocess.check_call(["./JavaState.sh", csc])
-                #subprocess.check_call(["./JavaComCon.sh", csc])
+                subprocess.check_call([f"./{path}JavaPubSub.sh", csc])
+                subprocess.check_call([f"./{path}JavaEvents.sh", csc])
+                #subprocess.check_call([f"./{path}JavaState.sh", csc])
+                #subprocess.check_call([f"./{path}JavaComCon.sh", csc])
             if ('cpp' not in language) and ('java' not in language) and ('all' not in language):
                 complete=False
         print("\nCOMPLETED ALL test suites for ALL CSCs.") if complete else print("This was a " + str(language))
     elif csc in csc_array:
             if ('cpp' in language) or ('all' in language):
-                subprocess.check_call(["./CppPubSub.sh", csc])
-                subprocess.check_call(["./CppEvents.sh", csc])
-                subprocess.check_call(["./CppAuthlist.sh", csc])
-                #subprocess.check_call(["./CppState.sh", csc])
-                #subprocess.check_call(["./CppComCon.sh", csc])
+                subprocess.check_call([f"./{path}CppPubSub.sh", csc])
+                subprocess.check_call([f"./{path}CppEvents.sh", csc])
+                #subprocess.check_call([f"./CppState.sh", csc])
+                #subprocess.check_call([f"./CppComCon.sh", csc])
             if ('java' in language) or ('all' in language):
-                subprocess.check_call(["./JavaPubSub.sh", csc])
-                subprocess.check_call(["./JavaEvents.sh", csc])
-                subprocess.check_call(["./JavaAuthlist.sh", csc])
-                #subprocess.check_call(["./JavaState.sh", csc])
-                #subprocess.check_call(["./JavaComCon.sh", csc])
+                subprocess.check_call([f"./{path}JavaPubSub.sh", csc])
+                subprocess.check_call([f"./{path}JavaEvents.sh", csc])
+                #subprocess.check_call([f"./{path}JavaState.sh", csc])
+                #subprocess.check_call([f"./{path}JavaComCon.sh", csc])
             if ('cpp' not in language) and ('java' not in language) and ('all' not in language):
                 complete=False
             print("\nCOMPLETED all test suites for the " + csc + " CSC") if complete else print("This was a " + str(language))
@@ -62,7 +64,8 @@ def DefineArguments():
         required=False,
         choices=csc_list,
         default='all',
-        help='''For which CSC do you want to generate tests? (Default is ALL)''')
+        help='''For which CSC do you want to generate tests? (Default is ALL)''',
+    )
     parser.add_argument(
         '-l',
         '--language',
@@ -73,7 +76,14 @@ def DefineArguments():
         nargs='+',
         choices=['cpp', 'java', 'all', 'test'],
         default='all',
-        help='''Do you want to generate the C++ (cpp), or Java (java) tests? (Default is ALL)''')
+        help='''Do you want to generate the C++ (cpp), or Java (java) tests? (Default is ALL)''',
+    )
+    parser.add_argument(
+        '-d',
+        '--dds',
+        action="store_true",
+        help='''If True, build the DDS messaging tests, otherwise, build the Kafka messaging tests.''',
+    )
     args = parser.parse_args()
     return args
 
@@ -86,4 +96,4 @@ os.chdir(working_dir)
 
 # Now, generate the tests.
 if __name__ == "__main__":
-    GenerateTests(args.csc, args.language)
+    GenerateTests(args.csc, args.language, args.dds)
